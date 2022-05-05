b757fab91e701b9f56db5a5b163861be592db1b1
dogaozgon <dogaozgon891@gmail.com>
2022-05-04 21:14:51 -0700

    change web app title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
 	return (
 		<div className="layout">
 			<Head>
-				<title>Doga Store</title>
+				<title>Ozgon Headphones</title>
 			</Head>
 			<header>
 				<Navbar />
